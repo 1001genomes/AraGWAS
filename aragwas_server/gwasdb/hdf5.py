deab2c60ce3b3e85fb557a484c6356a57baba964
Togninalli  Matteo <matteo.togninalli@bsse.ethz.ch>
2017-06-15 12:06:44 +0200

    Deleted unnecessary function part

--- aragwas_server/gwasdb/hdf5.py
+++ aragwas_server/gwasdb/hdf5.py
@@ -45,59 +45,6 @@ def get_top_associations(hdf5_file, val=100, top_or_threshold='top'):
     return top_associations, thresholds
 
 
-    """Retrieves the top associations from an hdf5 file"""
-    if top_or_threshold == 'top': # retrieves top 'val' associations on each chromosome
-        top = True
-    elif top_or_threshold == 'threshold':
-        top = False
-        if val < 1:
-            val = -math.log(val, 10)
-        print(val)
-    else:
-        raise Warning('Please provide a valid option: top or threshold')
-
-    try:
-        association_file = h5py.File(hdf5_file, 'r')
-    except:
-        raise FileNotFoundError("Study file not found ({})".format(hdf5_file))
-
-    # Get SNP position in file
-    pval = []
-    pos = []
-    mafs = []
-    n_asso = 0
-    # TODO comnbine both branches. 1.) assume hdf5 file is sorted 2.) get the index which satitises predicate (top numbers or threshold) 3.) slice them out.
-    if top:
-        for i in range(5):
-            pval.append(association_file['pvalues']['chr'+str(i+1)]['scores'][:val])
-            pos.append(association_file['pvalues']['chr'+str(i+1)]['positions'][:val])
-            mafs.append(association_file['pvalues']['chr'+str(i+1)]['mafs'][:val])
-            n_asso += len(association_file['pvalues']['chr'+str(i+1)]['scores'])
-    else:
-        for i in range(5):
-            local_pval = []
-            local_pos = []
-            local_mafs = []
-            for j in range(10000):
-                if float(association_file['pvalues']['chr'+str(i+1)]['scores'][j]) >= val:
-                    local_pval.append(association_file['pvalues']['chr'+str(i+1)]['scores'][j])
-                    local_pos.append(association_file['pvalues']['chr' + str(i + 1)]['positions'][j])
-                    local_mafs.append(association_file['pvalues']['chr' + str(i + 1)]['mafs'][j])
-                else:
-                    # check next one and break:
-                    if float(association_file['pvalues']['chr' + str(i + 1)]['scores'][j+1]) < val:
-                        break
-            pval.append(local_pval)
-            pos.append(local_pos)
-            mafs.append(local_mafs)
-            n_asso += len(association_file['pvalues']['chr'+str(i+1)]['scores'])
-
-    bt05 = -math.log(0.05/float(n_asso), 10)
-    bt01 = -math.log(0.01/float(n_asso), 10)
-    thresholds = {'bonferoni_threshold05': bt05,'bonferoni_threshold01': bt01, 'total_associations': n_asso}
-
-    return pval, pos, mafs, n_asso, thresholds
-
 def regroup_associations(top_associations):
     """regroups associations from a hdf5 file to generate an ordered top list, not chromosome-specific"""
     top_associations.sort(order = 'score')
