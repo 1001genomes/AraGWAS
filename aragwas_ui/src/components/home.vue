428373d08fc27883b3508e69ab032ccff5245d3a
Togninalli  Matteo <matteo.togninalli@bsse.ethz.ch>
2017-06-28 10:56:09 +0200

    Changed displaying of title in home page

--- aragwas_ui/src/components/home.vue
+++ aragwas_ui/src/components/home.vue
@@ -5,10 +5,10 @@
                 <v-layout column align-center justify-center v-if="!search">
                     <div class="banner-title">
                         <br>
-                        <h1 class="white--text text-xs-center" >Ara<b>GWAS</b>Catalog</h1>
+                        <h1 class="white--text text-xs-center" >AraGWAS Catalog</h1>
                     </div>
                     <div class="banner-subtext">
-                        <h5 class="white--text text-xs-center">Ara<b>GWAS</b>Catalog is a public database catalog of <em>Arabidopsis thaliana</em> associations from published GWASs.</h5>
+                        <h5 class="white--text text-xs-center">AraGWAS Catalog is a public database catalog of <em>Arabidopsis thaliana</em> associations from published GWASs.</h5>
                         <br>
                         <h6 class="white--text light text-xs-center">This Database allows to search and filter for public GWASs, phenotypes and genes and to obtain additional meta-information. All GWASs were recomputed following a uniformed methodology to allow for comparable results.</h6>
                     </div>
