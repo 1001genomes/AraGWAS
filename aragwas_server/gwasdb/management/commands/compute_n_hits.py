df8aef9ec47e0acf02cf5add7407b556f4638442
Uemit Seren <uemit.seren@gmail.com>
2017-06-28 22:15:04 +0200

    Don't stop when a study HDF5 file is not found

--- aragwas_server/gwasdb/management/commands/compute_n_hits.py
+++ aragwas_server/gwasdb/management/commands/compute_n_hits.py
@@ -37,15 +37,18 @@ class Command(BaseCommand):
                 ids_aragwas = Study.objects.all().values_list('id', flat=True)
             counter = 0
             for id in ids_aragwas:
-                study = Study.objects.get(pk=id)
-                if study.n_hits_bonf == None or update_all: # Condition for first run through, might be changed to update all
-                    hdf5_file = os.path.join(settings.HDF5_FILE_PATH, '%s.hdf5' % study.pk)
-                    hits, thresholds = get_hit_count(hdf5_file, maf=maf)
-                    study.n_hits_bonf = hits['bonferroni_hits05']
-                    study.n_hits_top = hits['thr_e-4']
-                    study.n_hits_fdr = hits['bh_hits']
-                    study.save()
-                    counter +=1
+                try:
+                    study = Study.objects.get(pk=id)
+                    if study.n_hits_bonf == None or update_all: # Condition for first run through, might be changed to update all
+                        hdf5_file = os.path.join(settings.HDF5_FILE_PATH, '%s.hdf5' % study.pk)
+                        hits, thresholds = get_hit_count(hdf5_file, maf=maf)
+                        study.n_hits_bonf = hits['bonferroni_hits05']
+                        study.n_hits_top = hits['thr_e-4']
+                        study.n_hits_fdr = hits['bh_hits']
+                        study.save()
+                        counter +=1
+                except FileNotFoundError as err:
+                    self.stdout.write(self.style.ERROR('HDF5 file for study %s not found' % study))
             print(str(counter) + ' studies updated in the database.')
         except Exception as err:
             raise CommandError(
