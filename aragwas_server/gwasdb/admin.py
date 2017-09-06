dde397086cc20db75b47a8079d704e2bc16bde46
Uemit Seren <uemit.seren@gmail.com>
2017-09-06 17:05:50 +0200

    Added some admin models and created additional super user

--- aragwas_server/gwasdb/admin.py
+++ aragwas_server/gwasdb/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from gwasdb.models import Study, Genotype
+from gwasdb.models import Study, Genotype, Phenotype
 
 @admin.register(Study)
 class StudyAdmin(admin.ModelAdmin):
     list_display = ['name', 'transformation', 'genotype', 'method','publication']
+
+@admin.register(Genotype)
+class GenotypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'version']
+
+@admin.register(Phenotype)
+class PhenotypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description','date' ]
